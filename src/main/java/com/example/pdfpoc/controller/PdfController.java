0b8bd72a12d2c2cca0efc98d874448d9866340d1
ericlineback <elineback@gmail.com>
2020-06-11 12:21:30 -0600

    added text to path.

--- src/main/java/com/example/pdfpoc/controller/PdfController.java
+++ src/main/java/com/example/pdfpoc/controller/PdfController.java
@@ -4,6 +4,8 @@ import com.itextpdf.text.Document;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.ByteArrayOutputStream;
@@ -12,8 +14,8 @@ import java.util.Date;
 @RestController
 public class PdfController {
 
-    @GetMapping(value = "/createpdf" ,produces = "application/pdf")
-    public byte[] createPdf(){
+    @GetMapping(value = "/createpdf/{text}" , produces = "application/pdf")
+    public byte[] createPdf(@PathVariable String text){
         Document document = new Document();
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
@@ -21,6 +23,7 @@ public class PdfController {
             PdfWriter.getInstance(document, outputStream);
             document.open();
             document.add(new Paragraph("This is how you do ºª•¶∞¢£™"));
+            document.add(new Paragraph("This is your text: " + text));
             document.add(new Paragraph(new Date().toString()));
             //Add more content here
         }catch(Exception e){
